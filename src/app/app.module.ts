e0c1f4f980ed931fc5a3b68a702487cd40628866
Noam Honig <noam@fireflymigration.com>
2017-12-25 17:09:39 +0200

    added radweb module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RadWebModule } from 'radweb';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RadWebModule
   ],
   providers: [],
   bootstrap: [AppComponent]
