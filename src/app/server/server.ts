3a284cc2d75a9b8842ce25af4a4598e934d161f2
Noam Honig <noam@fireflymigration.com>
2017-12-25 16:55:05 +0200

    configured sql server on the server and the basic data api

--- src/app/server/server.ts
+++ src/app/server/server.ts
@@ -1,6 +1,16 @@
+import { environment } from './../../environments/environment';
 import * as express from 'express';
+import * as radweb from 'radweb';
+import { SQLServerDataProvider, ExpressBridge } from 'radweb/server';
 let app = express();
 let port = 3000;
 
+let sqlServer = new SQLServerDataProvider('sa', 'MASTERKEY', '127.0.0.1', 'northwind', 'sqlexpress');
+environment.dataSource = sqlServer;
+
+let eb = new ExpressBridge(app, '/dataApi');
+eb.addSqlDevHelpers(sqlServer);
+
 app.route('/').get((req, res) => res.send('hello world'));
+
 app.listen(port);
