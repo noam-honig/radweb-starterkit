063491d44005e1ed3e227b5197a7994dcdf4ede8
Noam <noam@fireflymigration.com>
2018-03-21 12:10:11 +0200

    Added routing to Starter Kit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
-import * as models from './models';
-import * as radweb from 'radweb';
+import { Component, transition } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
-  title = 'app';
+  isCollapsed = true;
+  constructor(public router: Router) { 
+    
+  }
+
 }
