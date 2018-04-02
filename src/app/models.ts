aad2bddd6bc5f083b5dd3af06504d03cab1c5929
Noam <noam@fireflymigration.com>
2018-04-02 12:45:25 +0300

    Fixed categories not to be minified

--- src/app/models.ts
+++ src/app/models.ts
@@ -2,17 +2,17 @@ import * as radweb from 'radweb';
 import { environment } from './../environments/environment';
 
 export class Categories extends radweb.Entity<number> {
-    id = new radweb.NumberColumn({dbName: 'CategoryID'});
-    categoryName = new radweb.StringColumn();
-    description = new radweb.StringColumn();
-    categoryNameLength = new radweb.NumberColumn({
-      virtualData:()=>this.categoryName.value.length
-    });
-    categoryNameLengthAsync = new radweb.NumberColumn({
-      virtualData:()=> Promise.resolve(this.categoryName.value.length)
-    });
-    constructor(settings?:radweb.EntityOptions) {
-        super(() => new Categories(settings), environment.dataSource,settings);
-        this.initColumns();
-    }
+  id = new radweb.NumberColumn({ dbName: 'CategoryID' });
+  categoryName = new radweb.StringColumn();
+  description = new radweb.StringColumn();
+  categoryNameLength = new radweb.NumberColumn({
+    virtualData: () => this.categoryName.value.length
+  });
+  categoryNameLengthAsync = new radweb.NumberColumn({
+    virtualData: () => Promise.resolve(this.categoryName.value.length)
+  });
+  constructor() {
+    super(() => new Categories(), environment.dataSource, 'Categories');
+    this.initColumns();
   }
+}
