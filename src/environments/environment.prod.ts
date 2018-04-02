a1d2a06ee830815ccc6733555ede953b98a68a9e
Noam <noam@fireflymigration.com>
2018-04-02 12:37:00 +0300

    Fxied prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+import * as radweb from 'radweb';
+const serverUrl= '/';
+
 export const environment = {
-  production: true
+  production: true,
+  serverUrl,
+  //dataSource: new LocalStorageDataProvider() as DataProviderFactory
+  dataSource : new radweb.RestDataProvider(serverUrl+ 'dataapi') as radweb.DataProviderFactory
 };
