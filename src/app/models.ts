490c22e74f2751ee6b3d25d0ce5ccc8b27f768b8
Noam <noam@fireflymigration.com>
2018-04-03 12:45:13 +0300

    removed unneded columns

--- src/app/models.ts
+++ src/app/models.ts
@@ -5,12 +5,7 @@ export class Categories extends radweb.Entity<number> {
   id = new radweb.NumberColumn({ dbName: 'CategoryID' });
   categoryName = new radweb.StringColumn();
   description = new radweb.StringColumn();
-  categoryNameLength = new radweb.NumberColumn({
-    virtualData: () => this.categoryName.value.length
-  });
-  categoryNameLengthAsync = new radweb.NumberColumn({
-    virtualData: () => Promise.resolve(this.categoryName.value.length)
-  });
+
   constructor() {
     super(() => new Categories(), environment.dataSource, 'Categories');
     this.initColumns();
